2f4b0aad74a587481af900c6c8ec8a87838e2b6c
Jeffrey Doto <jeffdoto@gmail.com>
2013-07-15 04:18:24 +0000

    adds dynamic string parse

--- web.js
+++ web.js
@@ -3,10 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var indexBuffer = fs.readFile('index.html');
+	response.send(indexBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
